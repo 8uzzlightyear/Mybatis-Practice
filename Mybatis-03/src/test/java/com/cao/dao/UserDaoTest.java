01b817340f2fcd0b2f3e8de3ffeab80867ca6ac2
buzzlightyear <keepcloseletyougo@163.com>
2023-06-05 13:12:56 +0800

    创建了03，学习了查找结果集映射

---
+++ Mybatis-03/src/test/java/com/cao/dao/UserDaoTest.java
@@ -0,0 +1,27 @@
+package com.cao.dao;
+
+import com.cao.pojo.User;
+import com.cao.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UserDaoTest {
+
+    @Test
+    public void test() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.getSqlSession();
+            UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+            User user1 = userMapper.getUserById(1);
+
+            System.out.println(user1);
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
